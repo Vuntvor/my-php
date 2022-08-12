8471f2a15e55a048dfb4652ba4f74b1e0176152f
Stanislav <serafimussps1@gmail.com>
2022-08-12 13:36:13 +0300

    Update category column

---
+++ database/migrations/2022_08_12_123000_update_shop_category_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('shop_category', function (Blueprint $table) {
+
+            $table->integer('parent_category')->nullable()->change();
+
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+
+    }
+};
